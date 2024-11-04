467cc32e1608db41284683da867f1cfb863735e4
GitHub Action <action@github.com>
2024-11-04 19:21:25 +0000

    Template cleanup

--- src/main/kotlin/com/github/tince250/intplugin/services/MyProjectService.kt
+++ src/main/kotlin/com/github/tince250/intplugin/services/MyProjectService.kt
@@ -1,9 +1,9 @@
-package org.jetbrains.plugins.template.services
+package com.github.tince250.intplugin.services
 
 import com.intellij.openapi.components.Service
 import com.intellij.openapi.diagnostic.thisLogger
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.tince250.intplugin.MyBundle
 
 @Service(Service.Level.PROJECT)
 class MyProjectService(project: Project) {
